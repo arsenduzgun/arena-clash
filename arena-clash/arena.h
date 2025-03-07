e7a4490e36e81181704f9401999885c04374fb0d
Arsen Duzgun <arsen.duzgun@hotmail.com>
2025-03-07 13:41:23 +0000

    arena.h improvements

--- arena-clash/arena.h
+++ arena-clash/arena.h
@@ -4,196 +4,135 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
 class Arena {
-
 public:
+    Arena() {}
 
-	Arena() {}
+    virtual void arenaEffect(vector<unique_ptr<Unit>>& team1, vector<unique_ptr<Unit>>& team2) = 0;
 
-	virtual void arenaEffect(vector<unique_ptr<Unit>>& team1, vector<unique_ptr<Unit>>& team2) = 0;
+    void battle(vector<unique_ptr<Unit>>& team1, vector<unique_ptr<Unit>>& team2) {
+        auto team1Iterator = team1.begin();
+        auto team2Iterator = team2.begin();
+        int round = 1;
+        bool isBattleOver = false;
 
-	void battle(vector<unique_ptr<Unit>>& team1, vector<unique_ptr<Unit>>& team2) {
-		vector<unique_ptr<Unit>>::iterator team1Iterator = team1.begin();
-		vector<unique_ptr<Unit>>::iterator team2Iterator = team2.begin();
-		int round = 1;
-		string input;
-		bool isAttackPerformed;
-		bool isAttackerAlive;
-		bool isBattleOver = false;
-		cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
-		while (!isBattleOver) {
-			cout << "\n\t\t\t\t\t\t### ROUND " << round << " ###" << endl;
-			teamsInfo(team1, team2);
-			cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
-			if (isTeamAlive(team1) && isTeamAlive(team2)) {
-				isAttackerAlive = false;
-				while (!isAttackerAlive) {
-					if (isAlive(*team1Iterator)) {
-						isAttackerAlive = true;
-						cout << "\nYou are leading " << (*team1Iterator)->getNickName() << ", which enemy do you want to attack, please enter a number: ";
-						isAttackPerformed = false;
-						while (!isAttackPerformed) {
-							cin >> input;
-							if (isInputValid(team2, input)) {
-								if (isAlive(team2[stoi(input) - 1])) {
-									isAttackPerformed = true;
-									(*team1Iterator)->attack(team1, team2, (*team1Iterator), team2[stoi(input) - 1]);
-									team1Iterator++;
-									if (team1Iterator == team1.end()) {
-										team1Iterator = team1.begin();
-									}
-								}
-								else {
-									cout << "\n" << team2[stoi(input) - 1]->getNickName() << " is already dead, please enter again: ";
-								}
-							}
-							else {
-								cout << "\nYou have entered an invalid value, please enter again: ";
-							}
-						}
-					}
-					else {
-						team1Iterator++;
-						if (team1Iterator == team1.end()) {
-							team1Iterator = team1.begin();
-						}
-					}
-				}
-			}
-			if (isTeamAlive(team1) && isTeamAlive(team2)) {
-				isAttackerAlive = false;
-				while (!isAttackerAlive) {
-					if (isAlive(*team2Iterator)) {
-						isAttackerAlive = true;
-						cout << "\nYou are leading " << (*team2Iterator)->getNickName() << ", which enemy do you want to attack, please enter a number: ";
-						isAttackPerformed = false;
-						while (!isAttackPerformed) {
-							cin >> input;
-							if (isInputValid(team1, input)) {
-								if (isAlive(team1[stoi(input) - 1])) {
-									isAttackPerformed = true;
-									(*team2Iterator)->attack(team2, team1, (*team2Iterator), team1[stoi(input) - 1]);
-									team2Iterator++;
-									if (team2Iterator == team2.end()) {
-										team2Iterator = team2.begin();
-									}
-									round++;
-								}
-								else {
-									cout << "\n" << team1[stoi(input) - 1]->getNickName() << " is already dead, please enter again: ";
-								}
-							}
-							else {
-								cout << "\nYou have entered an invalid value, please enter again: ";
-							}
-						}
-					}
-					else {
-						team2Iterator++;
-						if (team2Iterator == team2.end()) {
-							team2Iterator = team2.begin();
-						}
-					}
-				}
-			}
-			arenaEffect(team1, team2);
-			cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
-			if (!isTeamAlive(team1) || !isTeamAlive(team2)) {
-				isBattleOver = true;
-			}
-		}
-		winner(team1, team2);
-	}
+        cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
+        while (!isBattleOver) {
+            cout << "\n\t\t\t\t\t\t### ROUND " << round << " ###" << endl;
+            displayTeamsInfo(team1, team2);
+            cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
 
-	void winner(vector<unique_ptr<Unit>>& team1, vector<unique_ptr<Unit>>& team2) {
-		cout << "\n\t\t\t\t\t****************************************" << endl;
-		if (!isTeamAlive(team1) && !isTeamAlive(team2)) {
-			cout << "\n\t\tAll units of " << team1.front()->getTeamName() << " and " << team2.front()->getTeamName() << " are dead, there is no winner" << endl;
-		}
-		else if (isTeamAlive(team1)) {
-			cout << "\n\t\t\t\t\t\t" << team1.front()->getTeamName() << " wins the battle" << endl;
-			cout << "\n\t\t\t\t\t\t\tSURVIVORS" << endl;
-			cout << "\t\t\t\t\t\t\t_________" << endl;
-			for (unique_ptr<Unit>& unit : team1) {
-				if (unit->getHP() > 0) {
-					cout << "\n\t\t\t\t\t\t" << unit->getNickName() << " (" << unit->getClassName() << " - " << unit->getHP() << " HP)" << endl;
-				}
-			}
-		}
-		else if (isTeamAlive(team2)) {
-			cout << "\n\t\t\t\t\t\t" << team2.front()->getTeamName() << " wins the battle" << endl;
-			cout << "\n\t\t\t\t\t\t\t" << "SURVIVORS" << endl;
-			cout << "\t\t\t\t\t\t\t_________" << endl;
-			for (unique_ptr<Unit>& unit : team2) {
-				if (unit->getHP() > 0) {
-					cout << "\n\t\t\t\t\t\t" << unit->getNickName() << " (" << unit->getClassName() << " - " << unit->getHP() << " HP)" << endl;
-				}
-			}
-		}
-		cout << "\n\t\t\t\t\t****************************************" << endl;
-		cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
-	}
+            if (isTeamAlive(team1) && isTeamAlive(team2)) {
+                processTurn(team1Iterator, team1, team2, "You are leading ");
+            }
+            if (isTeamAlive(team1) && isTeamAlive(team2)) {
+                processTurn(team2Iterator, team2, team1, "You are leading ");
+                round++;
+            }
 
-	void teamsInfo(vector<unique_ptr<Unit>>& team1, vector<unique_ptr<Unit>>& team2) {
-		for (unique_ptr<Unit>& unit : team1) {
-			if (unit->getCreationOrder() == team1.front()->getCreationOrder()) {
-				cout << "\n" << unit->getTeamName() << ": " << unit->getNickName() << "(" << unit->getClassName() << " - " << unit->getHP() << " HP) | ";
-			}
-			else if (unit->getCreationOrder() == team1.back()->getCreationOrder()) {
-				cout << unit->getNickName() << "(" << unit->getClassName() << " - " << unit->getHP() << " HP)" << endl;
-			}
-			else {
-				cout << unit->getNickName() << "(" << unit->getClassName() << " - " << unit->getHP() << " HP) | ";
-			}
-		}
-		for (unique_ptr<Unit>& unit : team2) {
-			if (unit->getCreationOrder() == team2.front()->getCreationOrder()) {
-				cout << "\n" << unit->getTeamName() << ": " << unit->getNickName() << "(" << unit->getClassName() << " - " << unit->getHP() << " HP) | ";
-			}
-			else if (unit->getCreationOrder() == team2.back()->getCreationOrder()) {
-				cout << unit->getNickName() << "(" << unit->getClassName() << " - " << unit->getHP() << " HP)" << endl;
-			}
-			else {
-				cout << unit->getNickName() << "(" << unit->getClassName() << " - " << unit->getHP() << " HP) | ";
-			}
-		}
-	}
+            arenaEffect(team1, team2);
+            cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
 
-	bool isTeamAlive(vector<unique_ptr<Unit>>& team) {
-		for (unique_ptr<Unit>& unit : team) {
-			if (unit->getHP() > 0) {
-				return true;
-			}
-		}
-		return false;
-	}
+            isBattleOver = !isTeamAlive(team1) || !isTeamAlive(team2);
+        }
+        determineWinner(team1, team2);
+    }
 
-	bool isAlive(unique_ptr<Unit>& unit) {
-		if (unit->getHP() > 0) {
-			return true;
-		}
-		return false;
-	}
+protected:  // Changed from private to protected for access in derived classes
+    bool isTeamAlive(const vector<unique_ptr<Unit>>& team) const {
+        return any_of(team.begin(), team.end(), [](const unique_ptr<Unit>& unit) { return unit->getHP() > 0; });
+    }
 
-	bool isInputValid(vector<unique_ptr<Unit>>& team, string input) {
-		for (unique_ptr<Unit>& unit : team) {
-			if (input._Equal(to_string(unit->getCreationOrder()))) {
-				return true;
-			}
-		}
-		return false;
-	}
+    bool isAlive(const unique_ptr<Unit>& unit) const {  // Added missing function
+        return unit->getHP() > 0;
+    }
 
-	bool isLucky(int SpecialAbilityChance) {
-		srand(time(NULL));
-		int chance = 1 + (rand() % 99);
-		if (chance <= SpecialAbilityChance) {
-			return true;
-		}
-		return false;
-	}
+    bool isLucky(int SpecialAbilityChance) {
+        static random_device rd;
+        static mt19937 gen(rd());
+        uniform_int_distribution<> distr(1, 100);
+        return distr(gen) <= SpecialAbilityChance;
+    }
+
+private:
+    void processTurn(vector<unique_ptr<Unit>>::iterator& attacker, vector<unique_ptr<Unit>>& attackingTeam,
+        vector<unique_ptr<Unit>>& defendingTeam, const string& prompt) {
+        string input;
+        while (true) {
+            if ((*attacker)->getHP() > 0) {
+                cout << "\n" << prompt << (*attacker)->getNickName()
+                    << ", which enemy do you want to attack? Enter a number: ";
+
+                while (true) {
+                    cin >> input;
+                    int targetIndex;
+                    try {
+                        targetIndex = stoi(input) - 1;
+                        if (targetIndex >= 0 && targetIndex < defendingTeam.size() && defendingTeam[targetIndex]->getHP() > 0) {
+                            break;
+                        }
+                        else {
+                            cout << "\nInvalid choice or target is already dead. Please enter again: ";
+                        }
+                    }
+                    catch (...) {
+                        cout << "\nInvalid input. Please enter a valid number: ";
+                    }
+                }
+
+                (*attacker)->attack(attackingTeam, defendingTeam, *attacker, defendingTeam[stoi(input) - 1]);
+                attacker++;
+                if (attacker == attackingTeam.end()) {
+                    attacker = attackingTeam.begin();
+                }
+                return;
+            }
+            attacker++;
+            if (attacker == attackingTeam.end()) {
+                attacker = attackingTeam.begin();
+            }
+        }
+    }
+
+    void determineWinner(vector<unique_ptr<Unit>>& team1, vector<unique_ptr<Unit>>& team2) {
+        cout << "\n\t\t\t\t\t****************************************" << endl;
+        if (!isTeamAlive(team1) && !isTeamAlive(team2)) {
+            cout << "\n\t\tBoth teams are eliminated. No winner." << endl;
+        }
+        else {
+            auto& winningTeam = isTeamAlive(team1) ? team1 : team2;
+            cout << "\n\t\t\t\t\t\t" << winningTeam.front()->getTeamName() << " wins the battle!" << endl;
+            cout << "\n\t\t\t\t\t\t\tSURVIVORS" << endl;
+            cout << "\t\t\t\t\t\t\t_________" << endl;
+            for (auto& unit : winningTeam) {
+                if (unit->getHP() > 0) {
+                    cout << "\n\t\t\t\t\t\t" << unit->getNickName() << " (" << unit->getClassName()
+                        << " - " << unit->getHP() << " HP)" << endl;
+                }
+            }
+        }
+        cout << "\n\t\t\t\t\t****************************************" << endl;
+        cout << "\n------------------------------------------------------------------------------------------------------------------------" << endl;
+    }
+
+    void displayTeamsInfo(const vector<unique_ptr<Unit>>& team1, const vector<unique_ptr<Unit>>& team2) {
+        auto printTeam = [](const vector<unique_ptr<Unit>>& team) {
+            cout << "\n" << team.front()->getTeamName() << ": ";
+            for (size_t i = 0; i < team.size(); ++i) {
+                cout << team[i]->getNickName() << " (" << team[i]->getClassName() << " - " << team[i]->getHP() << " HP)";
+                if (i != team.size() - 1) cout << " | ";
+            }
+            cout << endl;
+            };
+        printTeam(team1);
+        printTeam(team2);
+    }
 };
+
 #endif
