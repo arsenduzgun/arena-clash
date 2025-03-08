0dee9ae38b683004455be10a6d13e40f1f9fb38f
Arsen Duzgun <arsen.duzgun@hotmail.com>
2025-03-08 00:24:06 +0000

    assassin.h improvements

--- arena-clash/assassin.h
+++ arena-clash/assassin.h
@@ -2,75 +2,76 @@
 #define ASSASSIN_H
 
 #include <iostream>
-#include "Unit.h"
 #include <thread>
 #include <vector>
+#include "Unit.h"
 
 using namespace std;
 
 class Assassin : public Unit {
-
 protected:
-
-	const int assassinHP = 250;
-	const int assassinMaxAttackPoint = 40;
-	const int assassinMinAttackPoint = 30;
-	const int assassinMaxDefencePoint = 15;
-	const int assassinMinDefencePoint = 10;
-	const int assassinSpecialAbilityChance = 15;
+    static constexpr int ASSASSIN_HP = 250;
+    static constexpr int ASSASSIN_MAX_ATTACK = 40;
+    static constexpr int ASSASSIN_MIN_ATTACK = 30;
+    static constexpr int ASSASSIN_MAX_DEFENSE = 15;
+    static constexpr int ASSASSIN_MIN_DEFENSE = 10;
+    static constexpr int ASSASSIN_SPECIAL_ABILITY_CHANCE = 15;
 
 public:
+    Assassin(const string& nickName, const string& className, const string& teamName, int creationOrder)
+        : Unit(nickName, className, teamName, creationOrder) {
+        HP = maxHP = ASSASSIN_HP;
+        maxAttackPoint = ASSASSIN_MAX_ATTACK;
+        minAttackPoint = ASSASSIN_MIN_ATTACK;
+        maxDefencePoint = ASSASSIN_MAX_DEFENSE;
+        minDefencePoint = ASSASSIN_MIN_DEFENSE;
+        specialAbilityChance = ASSASSIN_SPECIAL_ABILITY_CHANCE;
+    }
 
-	Assassin(string nickName, string className, string teamName, int creationOrder) : Unit(nickName, className, teamName, creationOrder) {
-		HP = assassinHP;
-		maxHP = assassinHP;
-		maxAttackPoint = assassinMaxAttackPoint;
-		minAttackPoint = assassinMinAttackPoint;
-		maxDefencePoint = assassinMaxDefencePoint;
-		minDefencePoint = assassinMinDefencePoint;
-		specialAbilityChance = assassinSpecialAbilityChance;
-	}
+    void attack(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam,
+        unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) override {
+        int damage = calculateDamage(attacker, defender);
+        applyDamage(defender, damage);
+        cout << "\n" << attacker->getNickName() << " deals " << damage << " damage to " << defender->getNickName() << endl;
+        specialSkill(attackersTeam, defendersTeam, attacker, defender);
+    }
 
-	void attack(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam, unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {
-		int damage = attacker->getAttackPoint() * (100.0 / (100 + defender->getDefencePoint()));
-		defender->setHP(defender->getHP() - damage);
-		cout << "\n" << attacker->getNickName() << " deals " << damage << " damage to " << defender->getNickName() << endl;
-		if (!isAlive(defender)) {
-			cout << "\n" << defender->getNickName() << " is dead" << endl;
-			defender->setHP(0);
-		}
-		else if (isAlive(defender)) {
-			cout << "\n" << defender->getNickName() << " has remaining " << defender->getHP() << " HP" << endl;
-		}
-		specialSkill(attackersTeam, defendersTeam, attacker, defender);
-	}
+    void specialSkill(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam,
+        unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) override {
+        if (!isAlive(attacker) || !isTeamAlive(defendersTeam)) return;
 
-	void specialSkill(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam, unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {
-		if (isAlive(attacker) && isTeamAlive(defendersTeam)) {
-			cout << "\n" << attacker->getNickName() << " tries to perform throwing knife attack" << endl;
-			this_thread::sleep_for(1s);
-			if (isLucky(attacker)) {
-				cout << "\n" << attacker->getNickName() << " is succeed to perform throwing knife attack" << endl;
-				int damage;
-				for (unique_ptr<Unit>& unit : defendersTeam) {
-					if (isAlive(unit)) {
-						damage = attacker->getAttackPoint() * (100.0 / (100 + unit->getDefencePoint()));
-						unit->setHP(unit->getHP() - damage);
-						cout << "\n" << attacker->getNickName() << " deals " << damage << " damage to " << unit->getNickName() << endl;
-						if (!isAlive(unit)) {
-							cout << "\n" << unit->getNickName() << " is dead" << endl;
-							unit->setHP(0);
-						}
-						else if (isAlive(unit)) {
-							cout << "\n" << unit->getNickName() << " has remaining " << unit->getHP() << " HP" << endl;
-						}
-					}
-				}
-			}
-			else {
-				cout << "\n" << attacker->getNickName() << " is failed to perform throwing knife attack" << endl;
-			}
-		}
-	}
+        cout << "\n" << attacker->getNickName() << " attempts a throwing knife attack..." << endl;
+        this_thread::sleep_for(1s);
+
+        if (isLucky(attacker)) {
+            cout << "\n" << attacker->getNickName() << " successfully performs a throwing knife attack!" << endl;
+            for (auto& unit : defendersTeam) {
+                if (isAlive(unit)) {
+                    int damage = calculateDamage(attacker, unit);
+                    applyDamage(unit, damage);
+                    cout << "\n" << attacker->getNickName() << " deals " << damage << " damage to " << unit->getNickName() << endl;
+                }
+            }
+        }
+        else {
+            cout << "\n" << attacker->getNickName() << " failed to perform the throwing knife attack." << endl;
+        }
+    }
+
+private:
+    int calculateDamage(unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {
+        return attacker->getAttackPoint() * (100.0 / (100 + defender->getDefencePoint()));
+    }
+
+    void applyDamage(unique_ptr<Unit>& defender, int damage) {
+        defender->setHP(max(0, defender->getHP() - damage));
+        if (!isAlive(defender)) {
+            cout << "\n" << defender->getNickName() << " has fallen!" << endl;
+        }
+        else {
+            cout << "\n" << defender->getNickName() << " has " << defender->getHP() << " HP remaining." << endl;
+        }
+    }
 };
-#endif
+
+#endif // ASSASSIN_H
