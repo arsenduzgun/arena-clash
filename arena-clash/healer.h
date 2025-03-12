b2ee27643a809d3ebe6709eaeb10cd830ab7051f
Arsen Duzgun <arsen.duzgun@hotmail.com>
2025-03-12 19:46:14 +0000

    improvements

--- arena-clash/healer.h
+++ arena-clash/healer.h
@@ -2,93 +2,83 @@
 #define HEALER_H
 
 #include <iostream>
-#include "Unit.h"
 #include <thread>
 #include <vector>
+#include "Unit.h"
 
 using namespace std;
 
 class Healer : public Unit {
-
 protected:
-	const int healerHP = 250;
-	const int healerMaxAttackPoint = 20;
-	const int healerMinAttackPoint = 15;
-	const int healerMaxDefencePoint = 20;
-	const int healerMinDefencePoint = 15;
-	const int healerSpecialAbilityChance = 15;
+    static constexpr int HEALER_HP = 250;
+    static constexpr int HEALER_MAX_ATTACK = 20;
+    static constexpr int HEALER_MIN_ATTACK = 15;
+    static constexpr int HEALER_MAX_DEFENSE = 20;
+    static constexpr int HEALER_MIN_DEFENSE = 15;
+    static constexpr int HEALER_SPECIAL_ABILITY_CHANCE = 15;
+    static constexpr double HEAL_PERCENTAGE = 0.2;
 
 public:
+    Healer(const string& nickName, const string& className, const string& teamName, int creationOrder)
+        : Unit(nickName, className, teamName, creationOrder) {
+        HP = maxHP = HEALER_HP;
+        maxAttackPoint = HEALER_MAX_ATTACK;
+        minAttackPoint = HEALER_MIN_ATTACK;
+        maxDefencePoint = HEALER_MAX_DEFENSE;
+        minDefencePoint = HEALER_MIN_DEFENSE;
+        specialAbilityChance = HEALER_SPECIAL_ABILITY_CHANCE;
+    }
 
-	Healer(string nickName, string className, string teamName, int creationOrder) : Unit(nickName, className, teamName, creationOrder) {
-		HP = healerHP;
-		maxHP = healerHP;
-		maxAttackPoint = healerMaxAttackPoint;
-		minAttackPoint = healerMinAttackPoint;
-		maxDefencePoint = healerMaxDefencePoint;
-		minDefencePoint = healerMinDefencePoint;
-		specialAbilityChance = healerSpecialAbilityChance;
-	}
+    void attack(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam,
+        unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) override {
+        int damage = calculateDamage(attacker, defender);
+        applyDamage(defender, damage);
+        cout << "\n" << attacker->getNickName() << " deals " << damage << " damage to " << defender->getNickName() << endl;
+        specialSkill(attackersTeam, defendersTeam, attacker, defender);
+    }
 
-	void attack(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam, unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {
-		int damage = attacker->getAttackPoint() * (100.0 / (100 + defender->getDefencePoint()));
-		defender->setHP(defender->getHP() - damage);
-		cout << "\n" << attacker->getNickName() << " deals " << damage << " damage to " << defender->getNickName() << endl;
-		if (!isAlive(defender)) {
-			cout << "\n" << defender->getNickName() << " is dead" << endl;
-			defender->setHP(0);
-		}
-		else if (isAlive(defender)) {
-			cout << "\n" << defender->getNickName() << " has remaining " << defender->getHP() << " HP" << endl;
-		}
-		specialSkill(attackersTeam, defendersTeam, attacker, defender);
-	}
+    void specialSkill(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam,
+        unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) override {
+        if (!isAlive(attacker) || !isTeamAlive(attackersTeam)) return;
 
+        cout << "\n" << attacker->getNickName() << " attempts a healing spell..." << endl;
+        this_thread::sleep_for(1s);
 
-	void specialSkill(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam, unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {
-		if (isAlive(attacker) && isTeamAlive(defendersTeam)) {
-			cout << "\n" << attacker->getNickName() << " tries to perform life god spell" << endl;
-			this_thread::sleep_for(1s);
-			if (isLucky(attacker)) {
-				cout << "\n" << attacker->getNickName() << " is succeed to perform life god spell" << endl;
-				int hp;
-				for (unique_ptr<Unit>& unit : attackersTeam) {
-					if (unit->getHP() >= ((unit->getMaxHP() * 4) / 5.0)) {
-						if (unit->getHP() == unit->getMaxHP()) {
-							cout << "\n" << unit->getNickName() << " has already full health" << endl;
-						}
-						else if (unit->getHP() != unit->getMaxHP()) {
-							hp = unit->getMaxHP() - unit->getHP();
-							unit->setHP(unit->getMaxHP());
-							if (unit->getCreationOrder() == attacker->getCreationOrder()) {
-								cout << "\n" << attacker->getNickName() << " heals himself/herself of " << hp << " HP and " << unit->getNickName() << " has " << unit->getHP() << " HP now" << endl;
-							}
-							else if (unit->getCreationOrder() != attacker->getCreationOrder()) {
-								cout << "\n" << attacker->getNickName() << " heals " << unit->getNickName() << " of " << hp << " HP and " << unit->getNickName() << " has " << unit->getHP() << " HP now" << endl;
-							}
-						}
-					}
-					else if (unit->getHP() < ((unit->getMaxHP() * 4) / 5.0)) {
-						if (isAlive(unit)) {
-							hp = (unit->getMaxHP() / 5.0);
-							unit->setHP(unit->getHP() + hp);
-							if (unit->getCreationOrder() == attacker->getCreationOrder()) {
-								cout << "\n" << attacker->getNickName() << " heals himself/herself of " << hp << " HP and " << unit->getNickName() << " has " << unit->getHP() << " HP now" << endl;
-							}
-							else if (unit->getCreationOrder() != attacker->getCreationOrder()) {
-								cout << "\n" << attacker->getNickName() << " heals " << unit->getNickName() << " of " << hp << " HP and " << unit->getNickName() << " has " << unit->getHP() << " HP now" << endl;
-							}
-						}
-						else if (!isAlive(unit)) {
-							cout << "\n" << unit->getNickName() << " cannot be healed, because " << unit->getNickName() << " is dead" << endl;
-						}
-					}
-				}
-			}
-			else {
-				cout << "\n" << attacker->getNickName() << " is failed to perform life god spell" << endl;
-			}
-		}
-	}
+        if (isLucky(attacker)) {
+            cout << "\n" << attacker->getNickName() << " successfully casts a healing spell!" << endl;
+            healTeam(attackersTeam, attacker);
+        }
+        else {
+            cout << "\n" << attacker->getNickName() << " failed to perform the healing spell." << endl;
+        }
+    }
+
+private:
+    int calculateDamage(unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {
+        return attacker->getAttackPoint() * (100.0 / (100 + defender->getDefencePoint()));
+    }
+
+    void applyDamage(unique_ptr<Unit>& defender, int damage) {
+        defender->setHP(max(0, defender->getHP() - damage));
+        if (!isAlive(defender)) {
+            cout << "\n" << defender->getNickName() << " has fallen!" << endl;
+        }
+        else {
+            cout << "\n" << defender->getNickName() << " has " << defender->getHP() << " HP remaining." << endl;
+        }
+    }
+
+    void healTeam(vector<unique_ptr<Unit>>& team, unique_ptr<Unit>& healer) {
+        for (auto& unit : team) {
+            if (isAlive(unit) && unit->getHP() < unit->getMaxHP()) {
+                int healAmount = static_cast<int>(unit->getMaxHP() * HEAL_PERCENTAGE);
+                unit->setHP(min(unit->getMaxHP(), unit->getHP() + healAmount));
+                cout << "\n" << healer->getNickName() << " heals " << unit->getNickName()
+                    << " for " << healAmount << " HP. " << unit->getNickName() << " now has "
+                    << unit->getHP() << " HP." << endl;
+            }
+        }
+    }
 };
+
 #endif
