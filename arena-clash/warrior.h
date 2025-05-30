88832591c88ef262714816a22f417583ccac6fcb
Arsen Duzgun <64609113+arsenduzgun@users.noreply.github.com>
2025-05-30 17:53:36 +0100

    Update warrior.h
    improvements

--- arena-clash/warrior.h
+++ arena-clash/warrior.h
@@ -2,63 +2,76 @@
 #define WARRIOR_H
 
 #include <iostream>
-#include "Unit.h"
 #include <thread>
 #include <vector>
+#include "Unit.h"
 
 using namespace std;
 
 class Warrior : public Unit {
 
 protected:
-	const int warriorHP = 200;
-	const int warriorMaxAttackPoint = 45;
-	const int warriorMinAttackPoint = 35;
-	const int warriorMaxDefencePoint = 15;
-	const int warriorMinDefencePoint = 10;
-	const int warriorSpecialAbilityChance = 30;
+    static constexpr int warriorHP = 200;
+    static constexpr int warriorMaxAttackPoint = 45;
+    static constexpr int warriorMinAttackPoint = 35;
+    static constexpr int warriorMaxDefencePoint = 15;
+    static constexpr int warriorMinDefencePoint = 10;
+    static constexpr int warriorSpecialAbilityChance = 30;
+
+    void applyDamage(unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender, int damage, bool isCritical) const {
+        defender->setHP(defender->getHP() - damage);
+        cout << "\n" << attacker->getNickName()
+             << " deals " << damage << " damage"
+             << (isCritical ? " (critical)" : "") << " to "
+             << defender->getNickName() << "." << endl;
+
+        if (!isAlive(defender)) {
+            defender->setHP(0);
+            cout << defender->getNickName() << " is dead." << endl;
+        } else {
+            cout << defender->getNickName() << " has " << defender->getHP() << " HP remaining." << endl;
+        }
+    }
 
 public:
+    Warrior(string nickName, string className, string teamName, int creationOrder)
+        : Unit(nickName, className, teamName, creationOrder) 
+    {
+        HP = warriorHP;
+        maxHP = warriorHP;
+        maxAttackPoint = warriorMaxAttackPoint;
+        minAttackPoint = warriorMinAttackPoint;
+        maxDefencePoint = warriorMaxDefencePoint;
+        minDefencePoint = warriorMinDefencePoint;
+        specialAbilityChance = warriorSpecialAbilityChance;
+    }
 
-	Warrior(string nickName, string className, string teamName, int creationOrder) : Unit(nickName, className, teamName, creationOrder) {
-		HP = warriorHP;
-		maxHP = warriorHP;
-		maxAttackPoint = warriorMaxAttackPoint;
-		minAttackPoint = warriorMinAttackPoint;
-		maxDefencePoint = warriorMaxDefencePoint;
-		minDefencePoint = warriorMinDefencePoint;
-		specialAbilityChance = warriorSpecialAbilityChance;
-	}
+    void attack(vector<unique_ptr<Unit>>& attackersTeam,
+                vector<unique_ptr<Unit>>& defendersTeam,
+                unique_ptr<Unit>& attacker,
+                unique_ptr<Unit>& defender) override 
+    {
+        int baseAttack = attacker->getAttackPoint();
+        int defence = defender->getDefencePoint();
+        int damage;
+        bool critical = isLucky(attacker);
 
-	void attack(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam, unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {
-		int damage;
-		if (isLucky(attacker)) {
-			damage = attacker->getAttackPoint() * (100.0 / (100 + defender->getDefencePoint())) * 2;
-			defender->setHP(defender->getHP() - damage);
-			cout << "\n" << attacker->getNickName() << " deals " << damage << " damage(critical) to " << defender->getNickName() << endl;
-			if (!isAlive(defender)) {
-				cout << "\n" << defender->getNickName() << " is dead" << endl;
-				defender->setHP(0); // If defenders HP drops below 0, this function sets defenders HP to 0 
-			}
-			else if (isAlive(defender)) {
-				cout << "\n" << defender->getNickName() << " has remaining " << defender->getHP() << " HP" << endl;
-			}
-		}
-		else {
-			damage = attacker->getAttackPoint() * (100.0 / (100 + defender->getDefencePoint()));
-			defender->setHP(defender->getHP() - damage);
-			cout << "\n" << attacker->getNickName() << " deals " << damage << " damage to " << defender->getNickName() << endl;
-			if (!isAlive(defender)) {
-				cout << "\n" << defender->getNickName() << " is dead" << endl;
-				defender->setHP(0);
-			}
-			else if (isAlive(defender)) {
-				cout << "\n" << defender->getNickName() << " has remaining " << defender->getHP() << " HP" << endl;
-			}
-		}
-	}
+        if (critical) {
+            damage = static_cast<int>(baseAttack * (100.0 / (100 + defence)) * 2);
+        } else {
+            damage = static_cast<int>(baseAttack * (100.0 / (100 + defence)));
+        }
 
-	void specialSkill(vector<unique_ptr<Unit>>& attackersTeam, vector<unique_ptr<Unit>>& defendersTeam, unique_ptr<Unit>& attacker, unique_ptr<Unit>& defender) {}
+        applyDamage(attacker, defender, damage, critical);
+    }
+
+    void specialSkill(vector<unique_ptr<Unit>>& attackersTeam,
+                      vector<unique_ptr<Unit>>& defendersTeam,
+                      unique_ptr<Unit>& attacker,
+                      unique_ptr<Unit>& defender) override 
+    {
+        // No special skill implemented yet for Warrior
+    }
 };
 
-#endif
+#endif // WARRIOR_H
